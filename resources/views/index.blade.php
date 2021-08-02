7957933a3b39f416a34895c10680026d1ce73c3d
Jerome Lalnunfima <jeromemi7k143@gmail.com>
2021-08-02 05:43:08 -0700

    nothing

---
+++ resources/views/index.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="{{ URL::asset('css/app.css') }}">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <div class="container">
+        <div class="navbar">
+            <div class="logo"><a href="#">Foodle</a></div>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Profile</a></li>
+                <li><a href="#">Browse</a></li>
+                <li><a href="#">Post</a></li>
+                <li><a href="#">About us</a></li>
+                <li><a href="#">Log in</a></li>
+            </ul>
+        </div>
+        <div class="slogan"><h1>
+            Add a joy of best <br>taste.</br>
+        </h1></div>
+        <div class="sloganext">About 100+ Sellers bring out their delicious food <br>to be delivered at your doorstep.</br></div>
+    </div>
+</body>
+</html>
